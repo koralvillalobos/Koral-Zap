6d4220579a7b0819644beed2ca7805000c0aa458
zaphodschmidt <51718047+zaphodschmidt@users.noreply.github.com>
2021-11-25 16:21:38 -0700

    Update GameTestDriver.cpp

--- GameTestDriver.cpp
+++ GameTestDriver.cpp
@@ -1,38 +1,68 @@
-#include "Player.h"
 #include "Action.h"
-#include "Hacker.h"
 #include "BB.h"
+#include "Hacker.h"
 #include "Map.h"
-#include <fstream>
-#include <iostream>
-#include <ctype.h>
-#include <iomanip>
-#include <cmath>
-#include <string>
-#include <fstream>
-#include <vector>
-#include <ctime>
+#include "Player.h"
+#include "Server.h"
 
 using namespace std;
 
-
-
 int main()
 {
-    Action action;
-    BB bb;
-    Hacker hacker;
-    Player player;
 
-    srand(time(NULL));
-    int pick;
-    string type = "tie";
+    Map map;       // create object called map of type Map
+    Player player; // create object called map of type Player
+    Hacker hacker; // create object called map of type Hacker
+    Action action; // create object called map of type Action
+    Server server; // create object called map of type server
 
-    while (type == "tie")
+    map.spawnBestBuy(1, 3);
+    map.spawnNPC(3, 8);
+    map.spawnHacker(1, 1);
+
+    char move;         // for storing user input
+    int option;        //KORAL MAKE SURE TO USE THIS FOR ANY MENU OPTIONS THAT USE NUMBERS INSTEAD OF LETTERS AS A SELECTION TOOL
+    bool quit = false; //if true stops game
+
+    while (quit == false)
     {
-        cout << "Enter a number for rock paper scissors\n1.Rock\n2.Paper\n3.Scissors" << endl;
-        cin >> pick;
-        type = action.RockPaperScissors(pick);
-        cout<<type<<endl;
+        map.displayMap(); // pretty print map_data in terminal
+        action.virus(player);
+        cout << "Valid moves are: " << endl;
+        map.displayMoves(); // give user a menu of valid moves to pick from
+
+        cout << "Input a move: ";
+        cin >> move;
+        cout << endl;
+        map.executeMove(move); // move the player on map based on user input
+
+        if (map.isBestBuyLocation())//Best Buy
+        {
+            cout << "You're in a Best Buy!" << endl;
+        }
+        if (map.isHackerLocation()) //Starts Hacker Action
+        {
+            bool stop = false;
+            action.displayHackerMenu();
+
+            while (stop == false)
+            {
+                cout << "Input an option: " << endl;
+                cin >> option;
+                cout << endl;
+                stop = action.executeHackerMenu(option, player, hacker);
+                cout << endl;
+            }
+        }
+        if (map.isNPCLocation()) //Starts NPC Action
+        {
+            cout << "You've encountered an NPC!" << endl;
+        }
+
+        //Option Quit
+        if (move == 'q')
+        {
+            quit = action.quitGame(move); //Option to quit game
+        }
     }
 }
