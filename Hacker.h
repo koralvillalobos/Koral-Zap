8b6eaac6a6c10579ab5dc3085404d9c126e6e086
zaphodschmidt <51718047+zaphodschmidt@users.noreply.github.com>
2021-11-25 16:22:39 -0700

    Update Hacker.h

--- Hacker.h
+++ Hacker.h
@@ -1,3 +1,8 @@
+// CSCI1300 Fall 2021
+// Authors: Zaphod Schmidt and Koral Villlalobos
+// Recitations: 220 - Pragna Mandadi and 315 - Teo Pice-Broncucia
+// Project 3 - Hacker Class
+
 #ifndef Hacker_H
 #define Hacker_H
 
@@ -9,30 +14,28 @@
 #include <string>
 #include <fstream>
 
+using namespace std;
 
-using namespace std; 
+class Hacker
+{
+private:
+    int challegeLevel;
+    string hackerName;
+    int rows;
+    int cols;
 
-class Hacker{
-    private:
-        int challegeLevel;
-        string hackerName;
-        int rows;
-        int cols;
+public:
+    Hacker();
+    string getHacker(); //name
+    void setHacker(string);
 
+    int getChallegeLevel();
+    void setChallegeLevel(int);
 
-    public:
-        Hacker();
-        string getHacker(); //name
-        void setHacker(string);
-
-        int getChallegeLevel();
-        void setChallegeLevel(int);
-
-        int getLocationRows();
-        int getLocationCols();
-        void setLocation(int, int);
-
-
+    int getLocationRows();
+    int getLocationCols();
+    void setLocation(int, int);
+    void pickHackerName(int);
 };
 
 #endif
