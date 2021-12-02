6a7090b2d079f0e9ea1860304309ef789044db3c
koralvillalobos <93951810+koralvillalobos@users.noreply.github.com>
2021-12-02 15:11:27 -0700

    Update Server.cpp

--- Server.cpp
+++ Server.cpp
@@ -1,4 +1,4 @@
-// CSCI1300 Fall 2021
+/// CSCI1300 Fall 2021
 // Authors: Zaphod Schmidt and Koral Villlalobos
 // Recitations: 220 - Pragna Mandadi and 315 - Teo Pice-Broncucia
 // Project 3 - Server Implimentation
@@ -11,26 +11,14 @@ using namespace std;
 
 Server::Server()//default constructor 
 {
-    room = 0;
-    hackersDefeated = 0;
+    room = 0; //set room number to 0
 }
 
 int::Server::getRoom(){
-    return room;
+    return room; //return server room number
 }
 
-void::Server::setRoom(int newRoom){
+void::Server::setRoom(int newRoom){ //change the server room number
     room = newRoom;
 }
 
-//print out menu in server rooms
-void::Server::printMenu(){
-    cout << "1. Fight a Hacker\n2. Speak to NPC (non-player character)\n3. Repair your computer\n4. Use your antivirus software \n5. Travel the Server Room\n6. Browse StackOverflow\n7. Quit" << endl;
-}
-
-int::Server:: getHackerDefeatCount(){
-    return hackersDefeated;
-}
-void ::Server::setHackerDefeatCount(int in_hackersDefeated){
-    hackersDefeated = in_hackersDefeated;
-}
