9290d231777f8e0619501d49c9f1ad0b11e7ad4a
koralvillalobos <93951810+koralvillalobos@users.noreply.github.com>
2021-12-02 16:28:04 -0700

    Update BB.cpp

--- BB.cpp
+++ BB.cpp
@@ -1,4 +1,4 @@
-// CSCI1300 Fall 2021
+/// CSCI1300 Fall 2021
 // Authors: Zaphod Schmidt and Koral Villlalobos
 // Recitations: 220 - Pragna Mandadi and 315 - Teo Pice-Broncucia
 // Project 3 - Best Buy Implimentation
@@ -7,7 +7,7 @@
 
 using namespace std;
 
-BB::BB() //default constructor
+BB::BB() //default constructor setting all numbers to 0 or others accordingly
 {
     computer = false;
     antiVirus = false;
@@ -30,15 +30,15 @@ BB::BB(bool newComputer, bool newAntiVirus, int newVPN, int newproviderLevel) //
     providerLevel = newproviderLevel;
 }
 
-void ::BB::printBBMenu()
+void ::BB::printBBMenu() //What you can buy at best buy
 {
     cout << "1. Computer Part\n2. Antivirus software\n3. Virtual Private Network (VPN)\n4. Internet Provider" << endl;
 }
-void ::BB::printStartMenu()
+void ::BB::printStartMenu() //this is the BB menue for the start of the game
 {
     cout << "- COMPUTER PARTS. If your computer breaks, you need 5\ncomputer parts to make a new one.\n- ANTIVIRUS SOFTWARE. If your computer is infected with a\nvirus, use the antivirus software to get rid of it.\n- VIRTUAL PRIVATE NETWORK (VPN). The more VPNs you have\nthe harder it is for a hacker to infect your computer with\na virus.\n- INTERNET PROVIDER. The better the internet provider, the\nmore reliable your hacking will be.\nYou can spend all of your money here before you start your\njourney, or you can save some to spend on a different\nelectronics site along the way. But beware, some of the\nwebsites online are shady, and they won’t always give you a\nfair price..." << endl;
 }
-double BB::findmultiplier(Server &server)
+double BB::findmultiplier(Server &server) //this function returns the multiplier for finding different prices at BB in different servers
 {
 
     int FUCKINGCOCKMONEY = server.getRoom();
@@ -207,7 +207,7 @@ void ::BB::setProviderLevel(int newProviderLevel)
 {
     providerLevel = newProviderLevel;
 }
-void ::BB::gameStart(Player &player, Server &server, Map &map, BB &bb)
+void ::BB::gameStart(Player &player, Server &server, Map &map, BB &bb) //this is the Best buy menu 
 {
     string bbOption;
     string purchaseAns;
@@ -219,7 +219,7 @@ void ::BB::gameStart(Player &player, Server &server, Map &map, BB &bb)
     cin >> purchaseAns;
 
     int i;
-    if (purchaseAns == "y" && player.getDogeCoin() > 0)
+    if (purchaseAns == "y" && player.getDogeCoin() > 0) //if the user wants to purchase and has doge 
     {
 
         do
@@ -229,7 +229,7 @@ void ::BB::gameStart(Player &player, Server &server, Map &map, BB &bb)
             cout << "Please input what you would like to buy." << endl;
             cin >> bbOption;
 
-            if (bbOption == "1")
+            if (bbOption == "1") //Computer parts menu
             {
                 int computerPartChoice;
                 string computerPartItem;
@@ -251,12 +251,12 @@ void ::BB::gameStart(Player &player, Server &server, Map &map, BB &bb)
                     cout << "1. CPU\n2. GPU\n3. Power Supply Unit\n4. Computer Case\n5. Internet card\n6. Keyboard and mouse\n7. Premade computer" << endl;
                     cin >> computerPartItem;
 
-                    if (computerPartItem == "1")
+                    if (computerPartItem == "1") //CPU
                     {
-                        if (player.getDogeCoin() >= (findmultiplier(server) * 10) && bb.getNumbCPU() < 3)
+                        if (player.getDogeCoin() >= (findmultiplier(server) * 10) && bb.getNumbCPU() < 3)//checking if enough doge and # specific part
                         {
-                            setNumbCPU(getNumbCPU() + 1);
-                            player.setDogeCoin(player.getDogeCoin() - (findmultiplier(server) * 10));
+                            setNumbCPU(getNumbCPU() + 1); //adding part
+                            player.setDogeCoin(player.getDogeCoin() - (findmultiplier(server) * 10)); //changing doge based on server room, prices increase with server
                             cout << "DogeCoin: " << player.getDogeCoin() << endl;
                             i++;
                         }
@@ -266,12 +266,12 @@ void ::BB::gameStart(Player &player, Server &server, Map &map, BB &bb)
                         }
                     }
 
-                    else if (computerPartItem == "2")
+                    else if (computerPartItem == "2") //GPU
                     {
-                        if (player.getDogeCoin() >= (findmultiplier(server) * 20) && bb.getNumbGPU() < 3)
+                        if (player.getDogeCoin() >= (findmultiplier(server) * 20) && bb.getNumbGPU() < 3)//checking if enough doge and # specific part
                         {
-                            setNumbGPU(getNumbGPU() + 1);
-                            player.setDogeCoin(player.getDogeCoin() - (findmultiplier(server) * 20));
+                            setNumbGPU(getNumbGPU() + 1);//adding part
+                            player.setDogeCoin(player.getDogeCoin() - (findmultiplier(server) * 20));//changing doge based on server room, prices increase with server
                             cout << "DogeCoin: " << player.getDogeCoin() << endl;
                             i++;
                         }
@@ -280,12 +280,12 @@ void ::BB::gameStart(Player &player, Server &server, Map &map, BB &bb)
                             cout << "Poor ass mf you do not have enough doge or already have 3 GPU" << endl;
                         }
                     }
-                    else if (computerPartItem == "3")
+                    else if (computerPartItem == "3") //POWER SUPPLY UNIT
                     {
-                        if (player.getDogeCoin() >= (findmultiplier(server) * 5) && bb.getNumbPowerSupplyUnit() < 3)
+                        if (player.getDogeCoin() >= (findmultiplier(server) * 5) && bb.getNumbPowerSupplyUnit() < 3)//checking if enough doge and # specific part
                         {
-                            setNumbPowerSupplyUnit(getNumbPowerSupplyUnit() + 1);
-                            player.setDogeCoin(player.getDogeCoin() - (findmultiplier(server) * 5));
+                            setNumbPowerSupplyUnit(getNumbPowerSupplyUnit() + 1);//adding part
+                            player.setDogeCoin(player.getDogeCoin() - (findmultiplier(server) * 5));//changing doge based on server room, prices increase with server
                             cout << "DogeCoin: " << player.getDogeCoin() << endl;
                             i++;
                         }
@@ -293,13 +293,13 @@ void ::BB::gameStart(Player &player, Server &server, Map &map, BB &bb)
                         {
                             cout << "Poor ass mf you do not have enough doge or already have 3 Power supply units" << endl;
                         }
-                    }
-                    else if (computerPartItem == "4")
+                    } 
+                    else if (computerPartItem == "4")//computer case
                     {
-                        if (player.getDogeCoin() >= (findmultiplier(server) * 15) && bb.getNumbComputerCase() < 3)
+                        if (player.getDogeCoin() >= (findmultiplier(server) * 15) && bb.getNumbComputerCase() < 3)//checking if enough doge and # specific part
                         {
-                            setNumbComputerCase(getNumbComputerCase() + 1);
-                            player.setDogeCoin(player.getDogeCoin() - (findmultiplier(server) * 15));
+                            setNumbComputerCase(getNumbComputerCase() + 1);//adding part
+                            player.setDogeCoin(player.getDogeCoin() - (findmultiplier(server) * 15));//changing doge based on server room, prices increase with server
                             cout << "DogeCoin: " << player.getDogeCoin() << endl;
                             i++;
                         }
@@ -309,12 +309,12 @@ void ::BB::gameStart(Player &player, Server &server, Map &map, BB &bb)
                         }
                     }
 
-                    else if (computerPartItem == "5")
+                    else if (computerPartItem == "5") //internet card
                     {
-                        if (player.getDogeCoin() >= (findmultiplier(server) * 5) && bb.getNumbInternetCard() < 3)
+                        if (player.getDogeCoin() >= (findmultiplier(server) * 5) && bb.getNumbInternetCard() < 3)//checking if enough doge and # specific part
                         {
-                            setNumbInternetCard(getNumbInternetCard() + 1);
-                            player.setDogeCoin(player.getDogeCoin() - (findmultiplier(server) * 5));
+                            setNumbInternetCard(getNumbInternetCard() + 1);//adding part
+                            player.setDogeCoin(player.getDogeCoin() - (findmultiplier(server) * 5));//changing doge based on server room, prices increase with server
                             cout << "DogeCoin: " << player.getDogeCoin() << endl;
                             i++;
                         }
@@ -324,12 +324,12 @@ void ::BB::gameStart(Player &player, Server &server, Map &map, BB &bb)
                         }
                     }
 
-                    else if (computerPartItem == "6")
+                    else if (computerPartItem == "6") //keyboard mouse 
                     {
-                        if (player.getDogeCoin() >= (10 * findmultiplier(server)) && bb.getNumbKeyboardMouse() < 3)
+                        if (player.getDogeCoin() >= (10 * findmultiplier(server)) && bb.getNumbKeyboardMouse() < 3)//checking if enough doge and # specific part
                         {
-                            setNumbKeyboardMouse(getCostKeyboardMouse() + 1);
-                            player.setDogeCoin(player.getDogeCoin() - (10 * findmultiplier(server)));
+                            setNumbKeyboardMouse(getCostKeyboardMouse() + 1);//adding part
+                            player.setDogeCoin(player.getDogeCoin() - (10 * findmultiplier(server)));//changing doge based on server room, prices increase with server
                             cout << "DogeCoin: " << player.getDogeCoin() << endl;
                             i++;
                         }
@@ -339,12 +339,12 @@ void ::BB::gameStart(Player &player, Server &server, Map &map, BB &bb)
                         }
                     }
 
-                    else if (computerPartItem == "7")
+                    else if (computerPartItem == "7") //premade computer
                     {
-                        if (player.getDogeCoin() >= (findmultiplier(server) * 100) && bb.getNumbPremadeComp() < 1)
+                        if (player.getDogeCoin() >= (findmultiplier(server) * 100) && bb.getNumbPremadeComp() < 1)//checking if enough doge and no other computer
                         {
-                            setNumbPremadeComp(getNumbPremadeComp() + 1);
-                            player.setDogeCoin(player.getDogeCoin() - (findmultiplier(server) * 100));
+                            setNumbPremadeComp(getNumbPremadeComp() + 1);//adding part
+                            player.setDogeCoin(player.getDogeCoin() - (findmultiplier(server) * 100));//changing doge based on server room, prices increase with server
                             cout << "DogeCoin: " << player.getDogeCoin() << endl;
                             i++;
                         }
@@ -359,7 +359,7 @@ void ::BB::gameStart(Player &player, Server &server, Map &map, BB &bb)
                     }
                 } while (i != computerPartChoice && player.getDogeCoin() > 5);
             }
-            else if (bbOption == "2")
+            else if (bbOption == "2") //buy USB stick
             {
                 int antivirusUSBChoice;
                 cout << "One USB stick of antivirus software costs " << findmultiplier(server) * 10 << " Dogecoins and will give you a one-time use to get rid of any viruses on your computer." << endl;
@@ -367,10 +367,10 @@ void ::BB::gameStart(Player &player, Server &server, Map &map, BB &bb)
                 cout << "How many antivirus USB sticks would you like to purchase?" << endl;
                 cin >> antivirusUSBChoice;
 
-                if (player.getDogeCoin() >= ((findmultiplier(server) * 10) * (antivirusUSBChoice)))
+                if (player.getDogeCoin() >= ((findmultiplier(server) * 10) * (antivirusUSBChoice)))//checking if enough doge
                 {
-                    player.setDogeCoin(player.getDogeCoin() - ((findmultiplier(server) * 10) * (antivirusUSBChoice)));
-                    player.setantiVirusUSBcount(player.getantiVirusUSBcount() + antivirusUSBChoice);
+                    player.setDogeCoin(player.getDogeCoin() - ((findmultiplier(server) * 10) * (antivirusUSBChoice)));//changing doge based on server room, prices increase with server
+                    player.setantiVirusUSBcount(player.getantiVirusUSBcount() + antivirusUSBChoice);//adding part
                     cout << "DogeCoin: " << player.getDogeCoin() << endl;
                     cout << "Antivirus: " << player.getantiVirusUSBcount() << endl;
                 }
@@ -380,17 +380,17 @@ void ::BB::gameStart(Player &player, Server &server, Map &map, BB &bb)
                 }
             }
 
-            else if (bbOption == "3")
+            else if (bbOption == "3") //VPN
             {
                 cout << "A VPN costs " << (20 * findmultiplier(server)) << " Dogecoins each. VPNs reduce your chances of getting hacked by preventing hackers from tracking down your main computer. The increase in security maxes out at 2 VPNs" << endl;
 
                 cout << "How many VPNs would you like to purchase?" << endl;
                 cin >> VPNChoice;
 
-                if (player.getDogeCoin() >= ((20 * findmultiplier(server))) * (VPNChoice))
+                if (player.getDogeCoin() >= ((20 * findmultiplier(server))) * (VPNChoice))//checking if enough doge
                 {
-                    player.setDogeCoin(player.getDogeCoin() - ((20 * findmultiplier(server))) * (VPNChoice));
-                    player.setVPNsOwned(player.getVPNsOwned() + VPNChoice);
+                    player.setDogeCoin(player.getDogeCoin() - ((20 * findmultiplier(server))) * (VPNChoice));//changing doge based on server room, prices increase with server
+                    player.setVPNsOwned(player.getVPNsOwned() + VPNChoice);//adding part
                     cout << "DogeCoin: " << player.getDogeCoin() << endl;
                     cout << "VPN: " << player.getVPNsOwned() << endl;
                 }
@@ -400,7 +400,7 @@ void ::BB::gameStart(Player &player, Server &server, Map &map, BB &bb)
                     cout << "Poor ass mf you do not have enough doge or already have an extra premade computer" << endl;
                 }
             }
-            else if (bbOption == "4")
+            else if (bbOption == "4")//INTERNET LEVEL
             {
                 string internetLevelChoice;
                 i = 0;
@@ -412,10 +412,10 @@ void ::BB::gameStart(Player &player, Server &server, Map &map, BB &bb)
                 if (internetLevelChoice == "2")
                 {
 
-                    if (player.getDogeCoin() >= (10 * findmultiplier(server)))
+                    if (player.getDogeCoin() >= (10 * findmultiplier(server)))//checking if enough doge
                     {
-                        player.setDogeCoin(player.getDogeCoin() - (10 * findmultiplier(server)));
-                        player.setinternetProv(stoi(internetLevelChoice));
+                        player.setDogeCoin(player.getDogeCoin() - (10 * findmultiplier(server)));//changing doge based on server room, prices increase with server
+                        player.setinternetProv(stoi(internetLevelChoice));//adding LEVEL
                         i++;
                     }
                     else
@@ -425,10 +425,10 @@ void ::BB::gameStart(Player &player, Server &server, Map &map, BB &bb)
                 }
                 else if (internetLevelChoice == "3")
                 {
-                    if (player.getDogeCoin() >= (25 * findmultiplier(server)))
+                    if (player.getDogeCoin() >= (25 * findmultiplier(server)))//checking if enough doge
                     {
-                        player.setDogeCoin(player.getDogeCoin() - (25 * findmultiplier(server)));
-                        player.setinternetProv(stoi(internetLevelChoice));
+                        player.setDogeCoin(player.getDogeCoin() - (25 * findmultiplier(server)));//changing doge based on server room, prices increase with server
+                        player.setinternetProv(stoi(internetLevelChoice));//adding LEVEL
                         i++;
                     }
                     else
@@ -439,10 +439,10 @@ void ::BB::gameStart(Player &player, Server &server, Map &map, BB &bb)
 
                 else if (internetLevelChoice == "4")
                 {
-                    if (player.getDogeCoin() >= (40 * findmultiplier(server)))
+                    if (player.getDogeCoin() >= (40 * findmultiplier(server)))//checking if enough doge
                     {
-                        player.setDogeCoin(player.getDogeCoin() - (40 * findmultiplier(server)));
-                        player.setinternetProv(stoi(internetLevelChoice));
+                        player.setDogeCoin(player.getDogeCoin() - (40 * findmultiplier(server)));//changing doge based on server room, prices increase with server
+                        player.setinternetProv(stoi(internetLevelChoice));//adding LEVEL
                         i++;
                     }
                     else
@@ -453,10 +453,10 @@ void ::BB::gameStart(Player &player, Server &server, Map &map, BB &bb)
 
                 else if (internetLevelChoice == "5")
                 {
-                    if (player.getDogeCoin() >= (50 * findmultiplier(server)))
+                    if (player.getDogeCoin() >= (50 * findmultiplier(server))) //checking if enough doge
                     {
-                        player.setDogeCoin(player.getDogeCoin() - (50 * findmultiplier(server)));
-                        player.setinternetProv(stoi(internetLevelChoice));
+                        player.setDogeCoin(player.getDogeCoin() - (50 * findmultiplier(server)));//changing doge based on server room, prices increase with server
+                        player.setinternetProv(stoi(internetLevelChoice));//adding LEVEL
                         i++;
                     }
                     else
@@ -481,14 +481,14 @@ void ::BB::gameStart(Player &player, Server &server, Map &map, BB &bb)
                 cout << "Invalid choice. Try again." << endl;
             }
         } while (bbOption != "5");
-        map.setPlayerColPosition(map.getPlayerColPosition() + 1);
+        map.setPlayerColPosition(map.getPlayerColPosition() + 1); //changing location out of BB to end buying
     }
     else
     {
-        map.setPlayerColPosition(map.getPlayerColPosition() + 1);
+        map.setPlayerColPosition(map.getPlayerColPosition() + 1);//changing location out of BB to end buying
     }
 }
-int BB::getCompPartsAvailable(BB &bb)
+int BB::getCompPartsAvailable(BB &bb) //function to see parts avaliable
 {
     return (bb.getNumbCPU() + bb.getNumbGPU() + bb.getNumbPowerSupplyUnit() + bb.getNumbComputerCase() + bb.getNumbInternetCard() + bb.getNumbKeyboardMouse() + bb.getNumbPremadeComp());
 }
